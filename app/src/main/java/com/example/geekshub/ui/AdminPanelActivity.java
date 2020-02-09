93967fba9effbb1d8c29b33e0487785e6ba708f5
Zeyad3ly <zeyadaly15@gmail.com>
2020-02-09 13:52:07 +0200

    Added Admin Panel Fixed Some bugs

---
+++ app/src/main/java/com/example/geekshub/ui/AdminPanelActivity.java
@@ -0,0 +1,16 @@
+package com.example.geekshub.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.geekshub.R;
+
+public class AdminPanelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_panel);
+    }
+}
